5bf72b9b834a5b0e4fc2a9c1c3e8db44dcc82c37
Michael Shimeles <michaelwasihun96@gmail.com>
2024-10-16 23:29:59 -0400

    Remove cors

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,15 @@ import auth from "./auth";
 export const app = new Hono();
 
 // Add CORS middleware
-app.use("*", (c, next) => {
-  c.header(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000, https://demo.goshenpay.com"
-  );
-  c.header("Access-Control-Allow-Methods", "POST, GET"); // Allow specific methods
-  c.header("Access-Control-Allow-Headers", "Content-Type"); // Allow specific headers
-  return next();
-});
+// app.use("*", (c, next) => {
+//   c.header(
+//     "Access-Control-Allow-Origin",
+//     "http://localhost:3000, https://demo.goshenpay.com,  https://goshenpay.com"
+//   );
+//   c.header("Access-Control-Allow-Methods", "POST, GET"); // Allow specific methods
+//   c.header("Access-Control-Allow-Headers", "Content-Type"); // Allow specific headers
+//   return next();
+// });
 
 // Home endpoint
 app.get("/", (c) => {
